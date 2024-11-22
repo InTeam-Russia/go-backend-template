a8aa87b4f06c44e66bb85f3b160ec2670b4dfa7a
tolstovr <robertproducts2021@gmail.com>
2024-11-22 10:45:33 +0400

    fix: CORS

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -3,17 +3,29 @@ package helpers
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func SetupCORS(r *gin.Engine) {
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	}))
+	r.Use(CORSMiddleware())
 }
 
 func CreateLogger(logLevel string) *zap.Logger {
